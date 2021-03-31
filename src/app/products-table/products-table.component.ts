bf594e92e65e4e5dc7dd71273236ad479970adf6
Felipe Sousa <csfelipe8@gmail.com>
2021-03-30 23:31:55 -0300

    Added Products Table

--- src/app/products-table/products-table.component.ts
+++ src/app/products-table/products-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../models/prdoduct.model';
 
 @Component({
   selector: 'app-products-table',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsTableComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  product_columns: string[] = [
+    "id",
+    "name",
+    "price",
+    "department",
+    "description"
+  ];
+  constructor(
+    private _productsService:ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.products = this._productsService.getProduct();
   }
 
 }
